f8fc05479c2bbdaf54d74fa2d45eecc5fb276243
xche03 <xche03@gmail.com>
2015-10-11 14:04:18 -0600

    Upload Exercise 1.3

---
+++ Ch1/Exercise 1.3.cpp
@@ -0,0 +1,9 @@
+/*Exercise 1.3: Write a program to print Hello, World on the standard
+output.*/
+#include <iostream>
+int main ()
+{
+	std::cout << "Hello, World" << std::endl;
+	system("PAUSE");
+	return 0;
+}
