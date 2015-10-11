8d54fcf9de0905e7589d923735af30dbbd2b0556
xche03 <xche03@gmail.com>
2015-10-11 13:39:58 -0600

    Upload Exercise 1.2

--- Ch1/Exercise 1.2.cpp
+++ Ch1/Exercise 1.2.cpp
@@ -1,10 +1,8 @@
 /*Exercise 1.2: Change the program to return -1. A return value of -1 is
 often treated as an indicator that the program failed. Recompile and rerun
 your program to see how your system treats a failure indicator from main.*/
-#include <iostream>
 int main()
 { 
-	system("PAUSE");
 	return -1;
 }
 /* use [echo %errorlevel%] to check the status */
