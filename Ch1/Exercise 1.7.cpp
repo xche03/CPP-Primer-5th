115a87784ffe11fb97845c971509b18a0e727088
Xiao Che <xche03@gmail.com>
2015-10-12 12:06:28 -0600

    Upload Exercise .17 nested comments

---
+++ Ch1/Exercise 1.7.cpp
@@ -0,0 +1,10 @@
+//Exercise 1.7: Compile a program that has incorrectly nested comments.
+/*
+* comment pairs /* */ cannot nest.
+* ''cannot nest'' is considered source code,
+*as is the rest of the program
+* /
+int main()
+{
+	return 0;
+}
