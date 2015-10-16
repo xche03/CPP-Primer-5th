2305505d7f650bbea4b721cc6e8056eb98e3b386
xche03 <xche03@gmail.com>
2015-10-15 19:48:38 -0600

    Exercise 2.2

---
+++ Ch2/Exercise 2.2.cpp
@@ -0,0 +1,4 @@
+/*Exercise 2.2: To calculate a mortgage payment, what types would you use
+for the rate, principal, and payment? Explain why you selected each type.*/
+
+/*I would use double type for all of them.*/
