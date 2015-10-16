eb104b685adb05ee94bd1bbd3851125ad16c0c9f
xche03 <xche03@gmail.com>
2015-10-15 19:39:41 -0600

    Exercise 2.1

---
+++ Ch2/Exercise 2.1.cpp
@@ -0,0 +1,6 @@
+/*Exercise 2.1: What are the differences between int, long, long long,
+and short? Between an unsigned and a signed type? Between a float and
+a double?*/
+/*The language guarantees that an int will be at least as large as short, a long at
+least as large as an int, and long long at least as large as long.The type long
+long was introduced by the new standard.*/
