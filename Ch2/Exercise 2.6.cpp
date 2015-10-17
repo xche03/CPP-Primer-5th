2d1dc7ab9a7c003023cbce976826942f9a9ad133
xche03 <xche03@gmail.com>
2015-10-16 19:42:15 -0600

    Exercise 2.6

---
+++ Ch2/Exercise 2.6.cpp
@@ -0,0 +1,13 @@
+/*Exercise 2.6: What, if any, are the differences between the following
+definitions :
+int month = 9, day = 7;
+int month = 09, day = 07;*/
+
+int main() {
+
+
+	int month = 9, day = 7;
+	int month1 = 09, day1 = 07 //error: invalid octal digit
+
+
+}
