a450fe8eb7379302d9f79c73162abfca38978c3d
xche03 <xche03@gmail.com>
2015-10-18 03:35:18 -0600

    Example 2.11

---
+++ Ch2/Exercise 2.11.cpp
@@ -0,0 +1,15 @@
+/*
+Exercise 2.11: Explain whether each of the following is a declaration or a
+definition :
+(a) extern int ix = 1024;
+(b) int iy;
+(c) extern int iz;*/
+
+
+//(a) extern int ix = 1024; //definition
+//(b) int iy;//declaration & definition
+//(c) extern int iz;//declaration
+
+/*A declaration makes a name known to the program.A file that wants to
+use a name defined elsewhere includes a declaration for that name.*/
+/*A definition creates the associated entity.*/
