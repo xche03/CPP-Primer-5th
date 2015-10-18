fa5f41be8860c0a8b3b72bf80db61cf257317510
xche03 <xche03@gmail.com>
2015-10-17 19:30:35 -0600

    Ex 2.9

---
+++ Ch2/Exercise 2.9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+	//std::cin >> int input_value;//error type name is not allowed.
+	//int i = { 3.14 };//list initialization Form of initialization that uses curly braces to enclose one or more initializers.
+	/*When used with variables of built-in type, this form of initialization has one
+important property: The compiler will not let us list initialize variables of built-in type if
+the initializer might lead to the loss of information:*/
+	//double salary = wage = 9999.99;//identifier "wage" is undefined
+	int i1 = 3.14;
+	return 0;
+
+
+}
