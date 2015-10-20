17274adf4ac2534074168d64464ccf96ec1929a7
xche03 <xche03@gmail.com>
2015-10-19 19:39:34 -0600

    Exercise 2.18

---
+++ Ch2/Exercise 2.18.cpp
@@ -0,0 +1,12 @@
+/*Exercise 2.18: Write code to change the value of a pointer. Write code to
+change the value to which the pointer points.*/
+
+int main() {
+	int *p1, *p2, i1 = 0, i2 = 0;
+	p1 = &i1;
+	p2 = &i2;
+	p1 = p2;
+	*p2 = 100;
+
+
+}
