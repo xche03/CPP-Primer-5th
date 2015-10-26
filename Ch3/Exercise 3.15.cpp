04c49fa872750ddf6c57f9bdf8a674c94e75e522
xche03 <xche03@gmail.com>
2015-10-25 19:24:23 -0600

    Ex3.15

---
+++ Ch3/Exercise 3.15.cpp
@@ -0,0 +1,16 @@
+/*Exercise 3.15: Repeat the previous program but read strings this time.*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+	vector<string> vi;
+	string s;
+	while (cin >> s) {
+		vi.push_back(s);
+	}
+
+
+
+}
