6eb20746f48a8fa428c6aab00ae48e572b41dc2f
Xiao Che <xche03@gmail.com>
2015-10-28 14:29:50 -0600

    Exercise 3.31

---
+++ Ch3/Exercise 3.31.cpp
@@ -0,0 +1,12 @@
+/*Exercise 3.31: Write a program to define an array of ten ints. Give each
+element the same value as its position in the array.*/
+
+#include <cstdlib>
+int main() {
+	constexpr size_t array_size = 10;
+	int a[10] = {};
+	for (size_t ix = 1; ix < array_size; ++ix) a[ix] = ix;
+
+	system("PAUSE");
+	return 0;
+}
