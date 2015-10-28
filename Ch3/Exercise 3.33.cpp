b098be000fce7a3a5f44ebdfd7e3f90bf9e30283
Xiao Che <xche03@gmail.com>
2015-10-28 14:56:18 -0600

    Exercise 3.33

---
+++ Ch3/Exercise 3.33.cpp
@@ -0,0 +1,3 @@
+/*Exercise 3.33: What would happen if we did not initialize the scores array
+in the program on page 116?*/
+/*Then for some values in the array will not be defined if no grades fall into the groups. */
