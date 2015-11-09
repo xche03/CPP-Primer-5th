d65a7331cc3d3eacea75f0b9c2554b3a9ed46ea5
Xiao Che <xche03@gmail.com>
2015-11-09 09:52:54 -0700

    Exercise 4.1

---
+++ Ch4/Exercise 4.1.cpp
@@ -0,0 +1,15 @@
+/*Exercise 4.1: What is the value returned by 5 + 10 * 20/2?*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	
+	cout << 5 + 10 * 20 / 2 << endl;
+
+	cout << 5 + ((10 * 20) / 2) << endl;
+
+	system("PAUSE");
+	return 0;
+}
