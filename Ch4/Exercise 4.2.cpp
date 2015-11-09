1a913a8289937b25d7527531b336b765508bb9a3
Xiao Che <xche03@gmail.com>
2015-11-09 10:03:02 -0700

    Exercise 4.2

---
+++ Ch4/Exercise 4.2.cpp
@@ -0,0 +1,7 @@
+/*Exercise 4.2: Using Table 4.12 (p. 166), parenthesize the following
+expressions to indicate the order in which the operands are grouped:
+(a) * vec.begin()
+(b) * vec.begin() + 1*/
+
+// (a) *(vec.begin())
+// (b) (*(vec.begin()))+1
