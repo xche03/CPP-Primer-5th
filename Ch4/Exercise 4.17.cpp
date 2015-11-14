57607b87a6a364d7883c56b3a68e26f3816b4984
xche03 <xche03@gmail.com>
2015-11-14 07:05:19 -0700

    Exercise 4.17

---
+++ Ch4/Exercise 4.17.cpp
@@ -0,0 +1,6 @@
+/*Exercise 4.17: Explain the difference between prefix and postfix increment.*/
+/*The increment (++) and decrement (--) operators provide a convenient notational
+shorthand for adding or subtracting 1 from an object.*/
+/*The postfix operators increment (or decrement) the
+operand but yield a copy of the original, unchanged value as its result.*/
+
