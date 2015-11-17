d15d96faefc53c8a07ec72247dff14f9152ef15d
xche03 <xche03@gmail.com>
2015-11-16 18:29:35 -0700

    Exerciese  4.27

---
+++ Ch4/Exercise 4.25.cpp
@@ -0,0 +1,9 @@
+/*
+Exercise 4.25: What is the value of ~'q' << 6 on a machine with 32-bit
+ints and 8 bit chars, that uses Latin-1 character set in which 'q' has the
+bit pattern 01110001?
+*/
+
+// before: 00000000 00000000 00000000 01110001
+//after    11111111 11111111 11111111 10001110
+
---
+++ Ch4/Exercise 4.26.cpp
@@ -0,0 +1,8 @@
+/*
+Exercise 4.26: In our grading example in this section, what would happen if
+we used unsigned int as the type for quiz1?
+*/
+
+/*
+We specified unsigned long because ints are only guaranteed to have 16 bits, and we need at least 17.
+*/
