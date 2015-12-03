bb62e8cc79ea90f99fb830fbaaf27dc18c52fd06
Xiao Che <xche03@gmail.com>
2015-12-03 10:56:05 -0700

    5.3

---
+++ Ch5/Exercise 5.3.cpp
@@ -0,0 +1,17 @@
+/*Exercise 5.3: Use the comma operator (§ 4.10, p. 157) to rewrite the
+while loop from § 1.4.1 (p. 11) so that it no longer requires a block. Explain
+whether this rewrite improves or diminishes the readability of this code.*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int sum = 0, val = 1;
+	// keep executing the while as long as val is less than or equal to 10
+	while (val <= 10) sum += val, ++val; 
+	
+	std::cout << "Sum of 1 to 10 inclusive is "
+		<< sum << std::endl;
+	system("PAUSE");
+	return 0;
+}
