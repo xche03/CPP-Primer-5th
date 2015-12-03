bf796df1a51ce9d0a67f9914743012ab922482a2
Xiao Che <xche03@gmail.com>
2015-12-03 10:45:28 -0700

    5.2

--- Ch5/Exercise 5.1.cpp
+++ Ch5/Exercise 5.1.cpp
@@ -1,14 +1,9 @@
-/*Exercise 4.38: Explain the following expression:
-Click here to view code image
-double slope = static_cast<double>(j/i);*/
+/*Exercise 5.1: What is a null statement? When might you use a null
+statement?*/
+/*The simplest statement is the empty statement, also known as a null statement. A
+null statement is a single semicolon:
+; // null statement*/
 
-#include <iostream>
-using namespace std;
-int main() {
-	int i = 6;
-	int j = 5;
-	double slope = static_cast<double>(j / i);
-	cout << slope << endl;
-	system("PAUSE");
-
-}
+/*A null statement is useful where the language requires a statement but the
+program’s logic does not. Such usage is most common when a loop’s work can be
+done within its condition.*/
---
+++ Ch5/Exercise 5.2.cpp
@@ -0,0 +1,6 @@
+/*Exercise 5.2: What is a block? When might you might use a block?*/
+/*A compound statement, usually referred to as a block, is a (possibly empty)
+sequence of statements and declarations surrounded by a pair of curly braces.*/
+
+/*Compound statements are used when the language requires a single statement but
+the logic of our program needs more than one.*/
