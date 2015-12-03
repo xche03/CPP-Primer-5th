77a988c93a887f24a48d4de89ec715cf66a6c723
Xiao Che <xche03@gmail.com>
2015-12-03 10:04:17 -0700

    4.38

---
+++ Ch4/Exercise 4.38.cpp
@@ -0,0 +1,16 @@
+/*Exercise 4.38: Explain the following expression:
+Click here to view code image
+double slope = static_cast<double>(j/i);*/
+
+#include <iostream>
+using namespace std;
+int main() {
+	int i = 5;
+	int j = 6;
+	double slope = static_cast<double>(j / i); 
+
+	cout<<slope<<
+
+	system("PAUSE");
+
+}
