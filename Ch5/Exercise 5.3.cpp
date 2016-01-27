7ee26766d64240c9125666b45ebadc3a2d8fbcf1
Xiao Che <xche03@gmail.com>
2016-01-27 12:55:22 -0700

    test

--- Ch5/Exercise 5.3.cpp
+++ Ch5/Exercise 5.3.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	int sum = 0, val = 1;
-	// keep executing the while as long as val is less than or equal to 10
+	// keep executing the while as long as val is less than or equal to 
 	while (val <= 10) sum += val, ++val; 
 	
 	std::cout << "Sum of 1 to 10 inclusive is "
